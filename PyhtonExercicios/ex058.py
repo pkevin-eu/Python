c6fb464335622c9d69314b624b54798207b302b0
Paulo Kevin <166461411+Paulokevin@users.noreply.github.com>
2024-08-06 15:09:41 -0300

    ..

--- PyhtonExercicios/ex058.py
+++ PyhtonExercicios/ex058.py
@@ -5,4 +5,7 @@ num = int(input('Digite o valor que você queira fazer o fatorial: '))
 while c > 1:
     print(c)
     c += 1
-print('FIM')
+print('FIM')from math import factorial
+n = int(input('Digite um número para calcular seu fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}'.format(n, f))
